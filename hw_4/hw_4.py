53cfcbd46df9670e225ad082f00c817c7cd3e340
alinatl <alinka99-t@mail.ru>
2017-11-20 21:09:05 +0300

    c

--- hw_4/hw_4.py
+++
@@ -1,9 +0,0 @@
-with open("text.txt", encoding="utf-8") as f: 
-    text = f.read() 
-    splited_text = text.split() 
-j=0 
-for i in splited_text: 
-    if i[0].isupper(): 
-        j+=1 
-answer=(j*100)//len(splited_text) 
-print(answer,"%")
